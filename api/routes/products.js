48d84206bc9d4c01193230177b39e4a46d868b8a
unknown <nguyenthuydung55555@gmail.com>
2024-02-21 16:41:36 +0700

    Add jsonwebtoken

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const multer = require("multer");
+const checkAuth = require("../middleware/check-auth");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -58,7 +59,7 @@ router.get("/", (req, res, next) => {
     });
 });
 
-router.post("/", upload.single("productImage"), (req, res, next) => {
+router.post("/", checkAuth, upload.single("productImage"), (req, res, next) => {
   console.log(req.file);
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
@@ -83,7 +84,7 @@ router.post("/", upload.single("productImage"), (req, res, next) => {
     });
 });
 
-router.get("/:productId", (req, res, next) => {
+router.get("/:productId", checkAuth, (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
     .exec()
